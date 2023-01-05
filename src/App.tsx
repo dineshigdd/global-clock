8675e20b5fb8f301ba2df63ac06d85901a57352a
unknown <dineshigdd@gmail.com>
2023-01-04 21:26:14 -0800

    move tempalte to App.js, modify css, add a fakeDB

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,17 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>local time: <span>{ Date()}</span></div>
+       <div>UTC time<span>Display UTC time</span></div>
+       <div>Foriegn time: 
+        <select>
+          <option>select a country</option>
+          <option>india</option>
+          <option>Uk</option>
+          <option>Argentina</option>
+        </select>
+       <span>Display the time of the foriegn coutry</span>
+       </div>
     </div>
   )
 }
